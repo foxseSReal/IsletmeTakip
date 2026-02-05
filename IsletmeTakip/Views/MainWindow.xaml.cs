5cc8089b5ac2dc01ecddf1de2332ae5a70e86b53
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2026-02-05 23:18:58 +0300

    Saat Eklendi.

--- IsletmeTakip/Views/MainWindow.xaml.cs
+++ IsletmeTakip/Views/MainWindow.xaml.cs
@@ -12,17 +12,29 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace IsletmeTakip
 {
     /// <summary>
     /// MainWindow.xaml etkileşim mantığı
     /// </summary>
+
+
+
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _saat.Text = DateTime.Now.ToString("HH:mm");
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
